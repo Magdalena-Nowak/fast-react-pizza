d21bd1988e030bd89a0fd8b438c54aa1b4f2cb7c
magdalena-nowak <magdalena.nowak@cytiva.com>
2025-08-12 17:04:47 +0200

    Added react router with data loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,62 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
 import { useState } from "react";
+
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Order, { loader as orderLoader } from "./features/order/Order";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
+import User from "./features/user/CreateUser";
+import Cart from "./features/cart/Cart";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      // {
+      //   path: "/user",
+      //   element: <User />,
+      // },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+        // loader: orderLoader,
+        // errorElement: <Error />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <>
+    <RouterProvider router={router}>
       <div className="box">Hello vite</div>
-    </>
+    </RouterProvider>
   );
 }
 
---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/searchOrder";
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <SearchOrder />
+      <p>Magda</p>
+    </header>
+  );
+}
+
+export default Header;
